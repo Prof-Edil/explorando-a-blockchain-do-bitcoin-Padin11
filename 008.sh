a880456e7c8192591a6a1f2354a65f268bf4d06f
Baldit <92806667+Padin11@users.noreply.github.com>
2025-02-03 00:41:34 -0300

    Mudança do índice

--- 008.sh
+++ 008.sh
@@ -1,5 +1,5 @@
 # Which public key signed input 0 in this tx:
 #   `e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163`
 tx="e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163"
-chave=$(bitcoin-cli getrawtransaction "$tx" true | jq -r '.vin[0].txinwitness[1]')
+chave=$(bitcoin-cli getrawtransaction "$tx" true | jq -r '.vin[0].txinwitness[2]')
   echo "${chave:4:66}"
