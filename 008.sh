76e6b52edf5c2bbe9509cf8646b1318ea99d94d0
Baldit <92806667+Padin11@users.noreply.github.com>
2025-02-03 00:38:35 -0300

    Update 008.sh

--- 008.sh
+++ 008.sh
@@ -1,2 +1,5 @@
 # Which public key signed input 0 in this tx:
 #   `e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163`
+tx="e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163"
+chave=$(bitcoin-cli getrawtransaction "$tx" true | jq -r '.vin[0].txinwitness[1]')
+  echo "${chave:4:66}"
